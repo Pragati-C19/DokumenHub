917045e01e0707e63ad9669cf4ec1c92bf2e06f9
Pragati Chothe <pragatichothe@gmail.com>
2024-09-27 22:50:00 +0530

    💄 login button is created

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -2,19 +2,14 @@
 
 'use client'; // Add this directive to make it a Client Component
 
-import { signIn } from "next-auth/react"; // Import signIn from NextAuth
+import LoginButton from "./login-button"
 
 const Header = () => {
-  const handleLogin = async () => {
-    await signIn("google"); // Trigger Google sign-in
-  };
 
   return (
     <header className="bg-gray-800 text-white p-4">
       <h1 className="text-xl">DokumenHub</h1>
-      <button onClick={handleLogin}>
-        Login with Google
-      </button>
+      <LoginButton />
     </header>
   );
 };
---
+++ app/hooks/useGoogleLogin.js
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "../utils/firebase-init";
+
+const useGoogleLogin = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const loginWithGoogle = () => {   
+    setLoading(true);
+    const provider = new GoogleAuthProvider();
+
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log("fn: loginWithGoogle(): result.user", result.user)
+
+        const userData = {
+          user_name: user.displayName,
+          email: user.email,
+          google_auth_id: user.uid,
+          profile_pic: user.photoURL,
+        };
+
+        return fetch('/api/auth/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(userData),
+        });
+      })
+      .then((response) => {
+        if (!response.ok) throw new Error("Failed to save user in the database.");
+        return response.json();
+      })
+      .catch((error) => {
+        setError(error.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return { loginWithGoogle, loading, error };
+};
+
+export default useGoogleLogin;
