8127133eda012da30a1b483f35b66440bd7b9f9a
Pragati Chothe <pragatichothe@gmail.com>
2024-09-28 20:09:23 +0530

    🗃️ changed to async function

--- app/utils/mysql-connection.js
+++ app/utils/mysql-connection.js
@@ -5,22 +5,19 @@ import dbConfig from "./db-config";
 const mysqlPool = mysql.createPool(dbConfig.mysql); // Use dbConfig.mysql for clarity
 
 // Function to check MySQL database connection
-const checkMySQLConnection = () => {
-  mysqlPool
-    .getConnection() // Attempt to get a connection from the pool
-    .then((connection) => {
-      console.log("Successfully connected to the MySQL database."); // Log success message
-      connection.release(); // Release the connection back to the pool
-    })
-    .catch((error) => {
-      console.error("Error connecting to the MySQL database:", error); // Log error message
-    })
-    .finally(() => {
-      console.log("MySQL connection attempt finished."); // Log that the connection attempt has finished
-    });
+const checkMySQLConnection = async () => {
+  try {
+    const connection = await mysqlPool.getConnection();
+    console.log("Successfully connected to the MySQL database."); // Log success message
+    connection.release(); // Release the connection back to the pool
+  } catch (error) {
+    console.log("Error connecting to the MySQL database."); // Log error message
+  } finally {
+    console.log("MySQL connection attempt finished."); // Log that the connection attempt has finished
+  }
 };
 
 // Call the function to check the MySQL connection (optional)
 checkMySQLConnection();
 
-export default mysqlPool; // Export the MySQL connection pool for use in other parts of the application
+export default checkMySQLConnection; // Export the MySQL connection pool for use in other parts of the application
