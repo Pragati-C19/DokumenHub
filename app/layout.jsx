237e2ad1c60c1a2086bc7ceaaa53d0326afd13eb
Pragati Chothe <pragatichothe@gmail.com>
2024-09-29 18:16:29 +0530

    💄 footer added on landing page

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import './styles/globals.css';
 import Header from './components/header'
+import Footer from './components/footer';
 
 const RootLayout = ({ children }) => {
   return (
@@ -12,13 +13,13 @@ const RootLayout = ({ children }) => {
           href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"
           rel="stylesheet"
         />
+        {/* For Footer Social Media Icons */}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
       </head>
-      <body className="bg-gray-100">
+      <body>
         <Header />
         <main>{children}</main>
-        <footer className="bg-gray-800 text-white p-4">
-          <p>&copy; 2024 DokumenHub</p>
-        </footer>
+        <Footer />
       </body>
     </html>
   );
